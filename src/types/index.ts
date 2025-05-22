22cade98dbcc21f3b8624c175f463d22638c14f2
josh <jarnld2@gmail.com>
2025-05-22 15:37:20 -0400

    Update package.json scripts and remove unused ConsumerBrand interface from types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,3 @@
-export interface ConsumerBrand {
-  id?: string;
-  name: string;
-  industry?: string;
-  retailer?: string;
-  deductions?: number;
-  last_reviewed?: string; // ISO date string
-}
-
 export type sortOptions = {
   columnName: string;
   direction: "asc" | "desc";
